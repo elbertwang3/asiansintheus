5b8f12f418a10c4448c8cb20420677eedd5e3b4c
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-10-25 21:32:22 -0700

    shifted down

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 
+
 var mapboxAccessToken = 'pk.eyJ1IjoiZWxiZXJ0d2FuZyIsImEiOiJjajk3dmw4amUwYmV2MnFydzl3NDIyaGFpIn0.46xwSuceSuv2Fkeqyiy0JQ';
 var countymap = L.map('countymap').setView([37.8, -96], 4);
-
+console.log(countyData);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapboxAccessToken, {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.light',
 }).addTo(countymap);
 
-L.geoJson(statesData).addTo(countymap);
+ Papa.parse('assets/data/wellston_zone_codes.csv', {
+ 	 header: true,
+    complete: function(results) {
+	countyData.eachLayer(function(layer) {
+		console.log(layer.feature.properties);
+	  featureJoinByProperty(layer.feature.properties, results.data, "PARCELID");
+	});
+L.geoJson(countyData).addTo(countymap);
