74f40264e342fdb54678168f466615cd531e3a74
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-10-26 15:39:17 -0700

    script gets all asians

--- apitocsv.py
+++ apitocsv.py
@@ -1,6 +1,8 @@
 import csv
 import requests
 import sys
+import json
+from pprint import pprint
 from census import Census
 from us import states
 import numpy as np
@@ -8,15 +10,45 @@ from collections import defaultdict
 from collections import OrderedDict
 
 def main(args):
-	print "HELLLLLLO"
-
+	with open('data/nationalcountydata/counties2.json') as data_file:    
+		data = json.load(data_file)
+	#pprint(data['features'][0])
+	asiansperyear = defaultdict(list)
 	apikey = '299c034012920f2e0a880131d68b369d0886ec1f'
 	url = args[1] + '&key=' + apikey
 	
-	r = requests.get(url)
-	print r.text
-	file1 = open('data/nationalcountydata/2009.csv', 'a')
-	writer = csv.writer(file1)
+	wadecounty = ('02','270')
+	kusilvak = ('02','158')
+	oglala = ('46', '102')
+	shannon = ('46', '113')
+	bedfordcity = ('51', '515')
+	bedfordcounty = ('51', '019')
+	for i in range(2009, 2016):
+		if (i == 2015):
+			url = 'https://api.census.gov/data/2015/acs/acs5?get=NAME,B02011_001E&for=county:*&in=state:*&key=' + apikey
+		else:
+			url = 'https://api.census.gov/data/' + str(i) + '/acs5?get=NAME,B02011_001E&for=county:*&in=state:*&key=' + apikey
+		r = requests.get(url)
+		r = r.json()
+		for j in range(1, len(r)):
+			row = r[j]
+			statecounty = (row[2], row[3])
+			if statecounty == wadecounty:
+				asiansperyear[kusilvak].append(int(row[1]))
+			elif statecounty == shannon:
+				asiansperyear[oglala].append(int(row[1]))
+			elif statecounty == bedfordcity:
+				asiansperyear[bedfordcounty][len(asiansperyear[bedfordcounty])-1] += (int(row[1]))
+			else:
+				asiansperyear[statecounty].append(int(row[1]))
+	for k,v in asiansperyear.iteritems():
+		print k,v
+
+	
+	
+
+	
+	
 
 
 if __name__ == "__main__":
--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 
 
 var mapboxAccessToken = 'pk.eyJ1IjoiZWxiZXJ0d2FuZyIsImEiOiJjajk3dmw4amUwYmV2MnFydzl3NDIyaGFpIn0.46xwSuceSuv2Fkeqyiy0JQ';
-var countymap = L.map('countymap').setView([37.8, -96], 4);
+var countymap = L.map('countymap').setView([39.8283, -98.5795], 4);
 console.log(countyData);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapboxAccessToken, {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.light',
 }).addTo(countymap);
+
+var LAcountymap = L.map('lacountymap').setView([34.05, -118.24], 10);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapboxAccessToken, {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.streets',
+}).addTo(LAcountymap);
 
  /*Papa.parse('assets/data/wellston_zone_codes.csv', {
  	 header: true,
@@ -25,4 +31,9 @@ function style(feature) {
         fillOpacity: 0.3
     };
 }
+
+
+
+console.log(lacounty);
 L.geoJson(countyData, {style: style}).addTo(countymap);
+L.geoJson(lacounty, {style: style}).addTo(LAcountymap);
