8ccb078a3e51e13b147350216fe240d2682c95f9
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-10-27 01:43:38 -0700

    legend an tooltip

--- apitocsv.py
+++ apitocsv.py
@@ -14,6 +14,8 @@ def main(args):
 		data = json.load(data_file)
 	#pprint(data['features'][0])
 	asiansperyear = defaultdict(list)
+	totalperyear = defaultdict(list)
+	percentperyear = defaultdict(list)
 	apikey = '299c034012920f2e0a880131d68b369d0886ec1f'
 	
 	
@@ -26,22 +28,46 @@ def main(args):
 	for i in range(2009, 2016):
 		if (i == 2015):
 			url = 'https://api.census.gov/data/2015/acs/acs5?get=NAME,B02011_001E&for=county:*&in=state:*&key=' + apikey
+			url2 = 'https://api.census.gov/data/2015/acs/acs5?get=NAME,B01001_001E&for=county:*&in=state:*&key=' + apikey
 		else:
 			url = 'https://api.census.gov/data/' + str(i) + '/acs5?get=NAME,B02011_001E&for=county:*&in=state:*&key=' + apikey
+			url2 = 'https://api.census.gov/data/' + str(i) + '/acs5?get=NAME,B01001_001E&for=county:*&in=state:*&key=' + apikey
 		r = requests.get(url)
+		r2 = requests.get(url2)
 		r = r.json()
+		r2 = r2.json()
 		for j in range(1, len(r)):
 			row = r[j]
 			statecounty = (row[2], row[3])
 			if statecounty == wadecounty:
-				asiansperyear[kusilvak].append(int(row[1]))
+				asiansperyear[kusilvak].append(float(row[1]))
 			elif statecounty == shannon:
 				asiansperyear[oglala].append(int(row[1]))
 			elif statecounty == bedfordcity:
 				asiansperyear[bedfordcounty][len(asiansperyear[bedfordcounty])-1] += (int(row[1]))
 			else:
 				asiansperyear[statecounty].append(int(row[1]))
+		for j in range(1, len(r2)):
+			row = r2[j]
+			statecounty = (row[2], row[3])
+			if statecounty == wadecounty:
+				totalperyear[kusilvak].append(float(row[1]))
+			elif statecounty == shannon:
+				totalperyear[oglala].append(int(row[1]))
+			elif statecounty == bedfordcity:
+				totalperyear[bedfordcounty][len(asiansperyear[bedfordcounty])-1] += (int(row[1]))
+			else:
+				totalperyear[statecounty].append(int(row[1]))
+
+	for k,v in totalperyear.iteritems():
+		if len(v) != 7:
+			print k,v
 	for k,v in asiansperyear.iteritems():
+		for j in range(len(v)):
+			percentperyear[k].append(float(asiansperyear[k][j])/float(totalperyear[k][j]))
+
+	
+	for k,v in percentperyear.iteritems():
 		print k,v
 
 
@@ -51,12 +77,12 @@ def main(args):
 		statefp = properties['STATEFP']
 		countyfp = properties['COUNTYFP']
 		for j in range(2009, 2016):
-			properties[str(j)] = asiansperyear[(statefp,countyfp)][j-2009]
+			properties[str(j)] = percentperyear[(statefp,countyfp)][j-2009]
 
-	pprint(data['features'])
+	#pprint(data['features'])
 
 
-	with open('data/nationalcountydata/countieswdata.json', 'w') as fp:
+	with open('data/nationalcountydata/countieswpercentdata.json', 'w') as fp:
 		json.dump(data, fp)
 
 
--- js/main.js
+++ js/main.js
@@ -21,24 +21,124 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
 		console.log(layer.feature.properties);
 	  featureJoinByProperty(layer.feature.properties, results.data, "PARCELID");
 	});*/
-var getColor = chroma.scale(['#451A53','#40968B','#FDE733']).domain([0,110000])
+function getColor(d) {
+	 return d > 0.3 ? '#ffffcc' :
+           d > 0.1  ? '#c7e9b4' :
+           d > 0.05 ? '#7fcdbb' :
+           d > 0.02  ? '#41b6c4' :
+           d > 0.01  ? '#2c7fb8' :
+                      '#253494';
+}
+
 function style(feature) {
     return {
-       fillColor: getColor(feature.properties['2009']),
-       //fillColor:'white',
-           color: 'white',
-    weight: 0,
-    opacity: 0,
-       fillOpacity: 1
-
-    };
+    	fillColor: getColor(feature.properties['2015']),
+        color: '#fff', // border color 
+        weight: 0.3,
+        fillOpacity: 1
+    }
 }
 
 
 
-console.log(lacounty);
-L.geoJson(countyData, {style: style}).addTo(countymap);
+console.log();
+var geojson;
+//L.geoJson(countyData, {style: style}).addTo(countymap);
+/*geojson = L.choropleth(percentData, {
+    valueProperty: '2015', // which property in the features to use 
+    scale: chroma.scale(['#2A4858','#fafa6e']).mode('lch').colors(5), // chroma.js scale - include as many as you like 
+    steps: 6, // number of breaks or steps in range 
+    mode: 'e', // q for quantile, e for equidistant, k for k-means 
+    style: {
+        color: '#fff', // border color 
+        weight: 0.3,
+        fillOpacity: 1
+    },
+    onEachFeature: function(feature, layer) {
+ 
+        layer.on({
+	        mouseover: highlightFeature,
+	        mouseout: resetHighlight,
+	        click: zoomToFeature
+	    });
+    }
+}).addTo(countymap)*/
+geojson = L.geoJson(percentData, {
+    style: style,
+    onEachFeature: function(feature, layer) {
+ 
+        layer.on({
+	        mouseover: highlightFeature,
+	        mouseout: resetHighlight,
+	        click: zoomToFeature
+	    });
+    }
+}).addTo(countymap)
 L.geoJson(lacounty, {style: style}).addTo(LAcountymap);
+
+
+
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 2.5,
+        color: '#666',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    info.update(layer.feature.properties);
+}
+
+function zoomToFeature(e) {
+    countymap.fitBounds(e.target.getBounds());
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Asian population percentage</h4>' +  (props ?
+        '<b>' + props['NAME'] + ' County' + '</b><br />' + props['2015'] * 100 + '% '
+        : 'Hover over a county');
+};
+
+info.addTo(countymap);
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 0.01, 0.02, 0.05, 0.1, 0.3],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i]+0.0001) + '"></i> ' +
+            grades[i] * 100 + "%" + (grades[i + 1] * 100 ? ' &ndash; ' + grades[i + 1] * 100 + "%" + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(countymap);
 
 
 
