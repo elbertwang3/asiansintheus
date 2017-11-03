d62c9e06164cc28292e5fbb6dd8c2b0ce5b8734e
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-11-03 00:18:41 -0700

    adding legend and hover

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
 }).addTo(SFcountymap);
 
 
-var NYcountymap = L.map('nycountymap').setView([40.720610, -73.935242], 11);
+var NYcountymap = L.map('nycountymap').setView([40.720610, -73.885242], 11);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapboxAccessToken, {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.light',
@@ -135,14 +135,14 @@ geojson3 = L.geoJson(sfcountypercent, {
 
 geojson4 = L.geoJson(nycountypercent, {
 	style: style4,
-	/*onEachFeature: function(feature, layer) {
+	onEachFeature: function(feature, layer) {
  
         layer.on({
-	        mouseover: highlightFeature3,
-	        mouseout: resetHighlight3,
-	        click: zoomToFeature3
+	        mouseover: highlightFeature4,
+	        mouseout: resetHighlight4,
+	        click: zoomToFeature4
 	    });
-    }*/
+    }
 }).addTo(NYcountymap);
 
 
@@ -238,9 +238,34 @@ function resetHighlight3(e) {
     info3.update();
 }
 
+function highlightFeature4(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 2.5,
+        color: '#666',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    info4.update(layer.feature.properties);
+}
+
+function zoomToFeature4(e) {
+    NYcountymap.fitBounds(e.target.getBounds());
+}
+
+function resetHighlight4(e) {
+    geojson3.resetStyle(e.target);
+    info4.update();
+}
+
 var info = L.control();
 var info2 = L.control();
 var info3 = L.control();
+var info4 = L.control();
 info.onAdd = function (map) {
     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
     this.update();
@@ -281,9 +306,24 @@ info3.update = function (props) {
         : 'Hover over a tract');
 };
 
+info4.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info4.update = function (props) {
+	console.log(props);
+    this._div.innerHTML = '<h4>Asian population percentage</h4>' +  (props ?
+        '<b>' + props['NTAName'] + '</b><br />' + props['2010'] * 100 + '% '
+        : 'Hover over a tract');
+};
+
 info.addTo(countymap);
 info2.addTo(LAcountymap);
 info3.addTo(SFcountymap);
+info4.addTo(NYcountymap);
 
 
 var legend = L.control({position: 'bottomright'});
@@ -338,9 +378,28 @@ legend3.onAdd = function (map) {
     return div;
 };
 
+var legend4 = L.control({position: 'bottomright'});
+
+legend4.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 0.01, 0.02, 0.05, 0.1, 0.25, 0.5, 0.75],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i]+0.0001) + '"></i> ' +
+            grades[i] * 100 + "%" + (grades[i + 1] * 100 ? ' &ndash; ' + grades[i + 1] * 100 + "%" + '<br>' : '+');
+    }
+
+    return div;
+};
+
 legend.addTo(countymap);
 legend2.addTo(LAcountymap);
 legend3.addTo(SFcountymap);
+legend4.addTo(NYcountymap);
 
 
 
