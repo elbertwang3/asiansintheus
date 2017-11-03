866d8d58f951fa013ebf6ad8dd0c1f99c970845d
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-11-02 17:52:10 -0700

    added SF and new scale

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 
 
 var mapboxAccessToken = 'pk.eyJ1IjoiZWxiZXJ0d2FuZyIsImEiOiJjajk3dmw4amUwYmV2MnFydzl3NDIyaGFpIn0.46xwSuceSuv2Fkeqyiy0JQ';
+
 var countymap = L.map('countymap').setView([39.8283, -98.5795], 4);
-console.log(countyData);
-
-
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapboxAccessToken, {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.light',
 }).addTo(countymap);
 
-var LAcountymap = L.map('lacountymap').setView([34.05, -118.24], 10);
+var LAcountymap = L.map('lacountymap').setView([34.05, -118.24], 11);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapboxAccessToken, {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    id: 'mapbox.streets',
+    id: 'mapbox.light',
 }).addTo(LAcountymap);
+
+var SFcountymap = L.map('sfcountymap').setView([37.7639, -122.4312], 11.5);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+mapboxAccessToken, {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.light',
+}).addTo(SFcountymap);
 
 var currentyear = 2015;
 
@@ -28,12 +32,14 @@ var currentyear = 2015;
 	  featureJoinByProperty(layer.feature.properties, results.data, "PARCELID");
 	});*/
 function getColor(d) {
-	 return d > 0.3 ? '#ffffcc' :
-           d > 0.1  ? '#c7e9b4' :
-           d > 0.05 ? '#7fcdbb' :
-           d > 0.02  ? '#41b6c4' :
-           d > 0.01  ? '#2c7fb8' :
-                      '#253494';
+	 return d > 0.75 ? '#ffffd9' :
+	 		d > 0.50 ? '#edf8b1' :
+	 		d > 0.25 ? '#c7e9b4' :
+           d > 0.1  ? '#7fcdbb' :
+           d > 0.05 ? '#41b6c4' :
+           d > 0.02  ? '#1d91c0' :
+           d > 0.01  ? '#225ea8' :
+                      '#0c2c84';
 }
 
 
@@ -56,12 +62,21 @@ function style2(feature) {
     }
 }
 
+function style3(feature) {
+    return {
+    	fillColor: getColor(feature.properties['2010']),
+        color: 'black', // border color 
+        weight: 0.2,
+        fillOpacity: 0.5
+    }
+}
 
 
 
 console.log();
 var geojson;
 var geojson2;
+var geojson3;
 
 //L.geoJson(countyData, {style: style}).addTo(countymap);
 /*geojson = L.choropleth(percentData, {
@@ -108,10 +123,21 @@ geojson2 = L.geoJson(lacountypercent, {
 }).addTo(LAcountymap);
 
 
+geojson3 = L.geoJson(sfcountypercent, {
+	style: style3,
+	onEachFeature: function(feature, layer) {
+ 
+        layer.on({
+	        mouseover: highlightFeature3,
+	        mouseout: resetHighlight3,
+	        click: zoomToFeature3
+	    });
+    }
+}).addTo(SFcountymap);
+
+
 
 var slider1 = L.control.slider(function(value) { 
-	console.log(value);
-
 	currentyear = value;
 	geojson.eachLayer(function(layer) {
 		 layer.setStyle({fillColor: getColor(layer.feature.properties[value.toString()]),})
@@ -179,8 +205,33 @@ function resetHighlight2(e) {
     info2.update();
 }
 
+function highlightFeature3(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 2.5,
+        color: '#666',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    info3.update(layer.feature.properties);
+}
+
+function zoomToFeature3(e) {
+    SFcountymap.fitBounds(e.target.getBounds());
+}
+
+function resetHighlight3(e) {
+    geojson3.resetStyle(e.target);
+    info3.update();
+}
+
 var info = L.control();
 var info2 = L.control();
+var info3 = L.control();
 info.onAdd = function (map) {
     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
     this.update();
@@ -207,8 +258,23 @@ info2.update = function (props) {
         : 'Hover over a tract');
 };
 
+info3.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info3.update = function (props) {
+	console.log(props);
+    this._div.innerHTML = '<h4>Asian population percentage</h4>' +  (props ?
+        '<b>' + props['namelsad10'] + '</b><br />' + props['2010'] * 100 + '% '
+        : 'Hover over a tract');
+};
+
 info.addTo(countymap);
 info2.addTo(LAcountymap);
+info3.addTo(SFcountymap);
 
 
 var legend = L.control({position: 'bottomright'});
@@ -216,7 +282,41 @@ var legend = L.control({position: 'bottomright'});
 legend.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 0.01, 0.02, 0.05, 0.1, 0.3],
+        grades = [0, 0.01, 0.02, 0.05, 0.1, 0.25, 0.5, 0.75],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i]+0.0001) + '"></i> ' +
+            grades[i] * 100 + "%" + (grades[i + 1] * 100 ? ' &ndash; ' + grades[i + 1] * 100 + "%" + '<br>' : '+');
+    }
+
+    return div;
+};
+var legend2 = L.control({position: 'bottomright'});
+
+legend2.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 0.01, 0.02, 0.05, 0.1, 0.25, 0.5, 0.75],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i]+0.0001) + '"></i> ' +
+            grades[i] * 100 + "%" + (grades[i + 1] * 100 ? ' &ndash; ' + grades[i + 1] * 100 + "%" + '<br>' : '+');
+    }
+
+    return div;
+};
+var legend3 = L.control({position: 'bottomright'});
+
+legend3.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 0.01, 0.02, 0.05, 0.1, 0.25, 0.5, 0.75],
         labels = [];
 
     // loop through our density intervals and generate a label with a colored square for each interval
@@ -230,8 +330,8 @@ legend.onAdd = function (map) {
 };
 
 legend.addTo(countymap);
-legend.addTo(LAcountymap);
-
+legend2.addTo(LAcountymap);
+legend3.addTo(SFcountymap);
 
 
 
--- tractstojson.py
+++ tractstojson.py
@@ -16,7 +16,7 @@ def main(args):
 	print statefips
 	print countyfips
 
-	with open('data/LAcountytractdata/lacounty.json') as data_file:    
+	with open('data/LAcountytractdata/sftracts.json') as data_file:    
 		data = json.load(data_file)
 	print pprint(data['features'][0])
 	asiansperyear = defaultdict(list)
@@ -24,9 +24,9 @@ def main(args):
 	percentperyear = defaultdict(list)
 	apikey = '299c034012920f2e0a880131d68b369d0886ec1f'
 
-	old = '930401'
-	merged = '137000'
-	for i in range(2010, 2016):
+	#old = '930401'
+	#merged = '137000'
+	for i in range(2010, 2011):
 		if (i == 2015):
 			url = 'https://api.census.gov/data/2015/acs/acs5?get=NAME,B02011_001E&for=tract:*&in=state:'+statefips+'%20county:' + countyfips + '&key=' + apikey
 			url2 = 'https://api.census.gov/data/2015/acs/acs5?get=NAME,B01001_001E&for=tract:*&in=state:'+statefips+'%20county:' + countyfips + '&key=' + apikey
@@ -42,25 +42,25 @@ def main(args):
 
 		for j in range(1, len(r)):
 			row = r[j]
-			if row[4] == old:
+			'''if row[4] == old:
 				asiansperyear[statefips + countyfips + merged].append(int(row[1]))
-			else: 
-				asiansperyear[statefips + countyfips + row[4]].append(int(row[1]))
+			else: '''
+			asiansperyear[statefips + countyfips + row[4]].append(int(row[1]))
 
 		for j in range(1, len(r2)):
 			row = r2[j]
-			if row[4] == old:
+			'''if row[4] == old:
 				totalperyear[statefips + countyfips + merged].append(int(row[1]))
-			else: 
-				totalperyear[statefips + countyfips + row[4]].append(int(row[1]))
+			else: '''
+			totalperyear[statefips + countyfips + row[4]].append(int(row[1]))
 
-	for k,v in asiansperyear.iteritems():
+	'''for k,v in asiansperyear.iteritems():
 		if len(v) != 6:
 			print k,v
 
 	for k,v in totalperyear.iteritems():
 		if len(v) != 6:
-			print k,v
+			print k,v'''
 
 	for k,v in asiansperyear.iteritems():
 		for j in range(len(v)):
@@ -72,14 +72,14 @@ def main(args):
 	for i in range(len(data['features'])):
 		county = data['features'][i]
 		properties = county['properties']
-		geoid = properties['external_id']
-		for j in range(2010, 2016):
+		geoid = properties['geoid10']
+		for j in range(2010, 2011):
 			properties[str(j)] = percentperyear[geoid][j-2010]
 
 	pprint(data['features'])
 
 
-	with open('data/LAcountytractdata/lacounty.json', 'w') as fp:
+	with open('data/LAcountytractdata/sfcounty2.json', 'w') as fp:
 		json.dump(data, fp)
 
 
